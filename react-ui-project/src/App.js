8d637cf9fbe159126eef6beeb766241a4dfddc3a
michae.lchristenson726@yahoo.com <121463561+mcchris1@users.noreply.github.com>
2023-04-25 11:31:26 -0400

    useEffectively

--- react-ui-project/src/App.js
+++ react-ui-project/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [selectedArtwork, setSelectedArtwork] = useState([])
+  const [searchTerm, setSearchTerm] = useState("french")
+
+  const fetchArt = () => {
+    fetch(`https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}&fields=id,title,artist_display,date_display,main_reference_number,image_id`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSelectedArtwork(data.data)
+        setSearchTerm("")
+      })
+  }
+
+  useEffect(() => {
+    fetchArt()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" onChange={(e)=> setSearchTerm(e.target.value)} value={searchTerm}/>
+        <button onClick={fetchArt}>Search</button>
+      </div>
+      <div>
+        {
+          selectedArtwork.map((artwork)=>(
+            <div key={artwork.id}>
+              <img src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`} />
+            </div>
+          ))
+        }
+      </div>
     </div>
+    
   );
 }
 
